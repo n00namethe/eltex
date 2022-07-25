f10130fc9afa1643a69c08df8ca95adc9e47b7ff
n00namethe <n00namethe@gmail.com>
2022-07-25 21:35:55 +0700

    Mas3

--- Mas/mas3.c
+++ Mas/mas3.c
@@ -4,25 +4,6 @@
 
 int main()
 {
-	// int i, j, x;
-	// i=j=0;
-	// int mas[N][N] = {};
-	// for (i=0; i<N; ++i)
-	// {
-	// 	for (j=0; j<N; ++j)
-	// 	{
-	// 		if (i<=j)
-	// 		{
-	// 			mas[i][j]=1;
-	// 		}
-	// 		else if (i>j)
-	// 		{
-	// 			mas[i][j]=0;
-	// 		}
-	// 		printf("%d", mas[i][j]);
-	// 	}
-	// 	printf("\n");
-	// }
 	int i, j, x;
 	i=j=0;
 	int mas[N][N] = {};
