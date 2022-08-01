68578c72f482f3d2f82f24f8b584a7bd18af6054
n00namethe <n00namethe@gmail.com>
2022-08-02 00:36:19 +0700

    adbook

--- adbook/adbook.c
+++ adbook/adbook.c
@@ -14,20 +14,20 @@ typedef struct
 int main()
 {
 	book adr[M]={0};
-	int j, z, x = 0; //Счетчики
-	char name;
-	int phone;
+	int i, z, x = 0; //Счетчики
+	int j; //switch
+	char name[N];
+	int phone, phs;
 	
-	int*t=&phone;
-	char*n=&name;
-
-	strncpy(adr[0].a, "Igor Prikhodko", N);
+	int*p=&phone;
+	
+	strncpy(adr[0].a, "igor prikhodko", N);
 	adr[0].c=74557;
 
-	strncpy(adr[1].a, "Ilya Trishin", N);
+	strncpy(adr[1].a, "ilya trishin", N);
 	adr[1].c=23453;
 
-	strncpy(adr[2].a, "Efim Glazkov", N);
+	strncpy(adr[2].a, "efim glazkov", N);
 	adr[2].c=73273;
 	for (int i=0; i<3; ++i)
 	{
@@ -42,8 +42,13 @@ int main()
 		printf("3. Найти абонента;\n");
 		printf("4. Удалить абонента;\n");
 		printf("5. Выход;\n");
-
-		scanf("%d%*c", &j);
+		int scn;
+		scn = scanf("%d%*c", &j);
+			if (scn == 0)
+			{
+				printf("You died\n");
+				return 0;
+			}
 		printf("\n");
 
 		switch(j)
@@ -51,87 +56,77 @@ int main()
 			
 				case 1: //добавление абонента
 					{
-						do
+						for (i=0; i<M; ++i)
 							{
-								int v = 0;
+								if (adr[i].c==0)
+								{
 								printf("Имя Фамилия:\n");
-								fgets(n, N, stdin);
-								strncpy(adr[v].a, n, N);
+								fgets(name, N, stdin);
+								name[strlen(name) - 1] = '\0';
+								strncpy(adr[i].a, name, N);
 								printf("Номер телефона:\n");
-								scanf("%d%*c", t);
-								adr[v].c = *t;
-								printf("Добавлен абонент:\n%s%d\n", adr[v].a, adr[v].c);
-								++v;
-								printf("Добавить еще?\n1. Да\n2. Нет\n");
-								scanf("%d%*c", &x);
-								
-
-							} while (x==1);
-								
-						break;
+								scanf("%d%*c", p);
+								adr[i].c = *p;
+								printf("Добавлен абонент:\n%s% d\n", adr[i].a, adr[i].c);
+								break;
+								}
+							}
 					}
-		
-			
+					break;
+					
 				case 2: //список абонентов
 					{
 					
-						for (int l=0; l<M; ++l)
+						for (i=0; i<M; ++i)
 							{
-								printf("%d. %s %d\n", z=l+1, adr[l].a, adr[l].c);
+								if (adr[i].c!=0)
+								{
+								printf("%d. %s %d\n", z=i+1, adr[i].a, adr[i].c);
+								}
 							}
-					
-
-						break;
 					}
+					break;
 
 				case 3: //поиск абонента
 					{
-						printf("Введите имя и фамилию\n");
-						fgets(n, N, stdin);
+					    printf("Номер телефона\n");
+						scanf("%d%*c", &phs);
 
-							for (int i=0; i<M; ++i)
+							for (i=0; i<M; ++i)
 							{
-								if (strcmp (n, adr[i].a)==0)
+								if (phs==adr[i].c)
 								{
-									printf("Имя фамилия: %s\n", adr[i].a);
-									printf("Номер телефона: %d\n", adr[i].c);
+									printf("Имя фамилия: %s\nНомер телефона: %d\n", adr[i].a, adr[i].c);
+									break;
 								}
 
-
 							}
-
-						break;							
-					
 					}
 
-
-
-
-
-
+					break;
 
 				case 4: //удаление абонента
 					{
 						int k;
 						printf("Выберете номер для удаления:\n");
 
-							for (int l=0; l<M; ++l)
+							for (i=0; i<M; ++i)
 							{
-								printf("%d. %s %d\n", z=l+1, adr[l].a, adr[l].c);
+								if (adr[i].c!=0)
+								{
+								printf("%d. %s %d\n", z=i+1, adr[i].a, adr[i].c);
+								}
 							}
 
 						scanf("%d%*c", &k);
 						--k;
 						strncpy(adr[k].a, "0", N);
 						adr[k].c=0;
-							for (int l=0; l<M; ++l)
-							{
-								printf("%d. %s %d\n", z=l+1, adr[l].a, adr[l].c);
-							}
-
-						break;
+											
 
 					}
+
+					break;
 
 				case 5: //Выход
 					{
