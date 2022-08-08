3ba061107da6e59fde7d83d49af7111cf6dc29e5
n00namethe <n00namethe@gmail.com>
2022-08-08 20:54:40 +0700

    Правки Кирилла

--- dinadbook/adbookd.c
+++ dinadbook/adbookd.c
@@ -6,30 +6,12 @@
 #include <string.h>
 #include <assert.h>
 
-/*int n;
-	printf("Enter size of array\n");
-	scanf("%d%*c", &n);
-	char*ptr1 = (char*)malloc(n);
-
-		if (NULL==ptr1)
-		{
-			printf("sold out\n");
-			return(1);
-		}
-
-			for (int i=0; i<n; ++i)
-			{
-				ptr1[i]=i;
-			}
-
-	printf ("you have a space\n");*/
-
 #define N 20
 
 typedef struct 
 {
-	char namesurname[N]; //name, second name
-	int  phonenum; 	   //phone number
+	char namesurname[N];
+	int  phonenum;
 } book;
 
 int main()
@@ -39,54 +21,44 @@ int main()
 	adr=malloc(M*sizeof(book));
 	if (NULL==adr)
 		{
-			printf("что-то не получилось\n");
+			printf("ошибка выделения памяти\n");
 			return(1);
 		}
-
 	int i; //Счетчик
 	int j; //switch
 	char name[N];
 	int p, phs;
 	
-	
 	int*phone=&p;
-	
-		strncpy(adr[0].namesurname, "igor prikhodko", N);
-		adr[0].phonenum=74557;
+	strncpy(adr[0].namesurname, "igor prikhodko", N);
+	adr[0].phonenum=74557;
 		
-		strncpy(adr[1].namesurname, "ilya trishin", N);
-		adr[1].phonenum=23453;
+	strncpy(adr[1].namesurname, "ilya trishin", N);
+	adr[1].phonenum=23453;
 		
-		strncpy(adr[2].namesurname, "efim glazkov", N);
-		adr[2].phonenum=73273;
-
-
-		
+	strncpy(adr[2].namesurname, "efim glazkov", N);
+	adr[2].phonenum=73273;
 	for (int i=0; i<M; ++i)
 	{
 		printf("%s %d\n", adr[i].namesurname, adr[i].phonenum);
 	}
-
-
 	while (1)
 	{
-
 		printf("1. Добавить абонента;\n");
 		printf("2. Список абонентов;\n");
 		printf("3. Найти абонента;\n");
 		printf("4. Удалить абонента;\n");
 		printf("5. Выход;\n");
 		int scn;
-		scn = scanf("%d%*c", &j);
-			if (scn == 0)
+		scn=scanf("%d%*c", &j);
+			if (scn!=1)
 			{
 				printf("You died\n");
 				return 0;
 			}
 		printf("\n");
-
 		switch(j)
-			{
+		{
 				case 1: //добавление абонента
 					{
 						++M;
@@ -94,17 +66,22 @@ int main()
 							if (adr==NULL)
 							{
 								free(adr);
-								printf("что-то случилось");
+								printf("ошибка выделения памяти");
 								break;
 							}
 						printf("Имя Фамилия:\n");
 						fgets(name, N-1, stdin);
-						//while ((name = getchar()) != '\n' && name != EOF);
 						name[strlen(name) - 1] = '\0';
+						//while ((getchar()!='\n') && (getchar() != EOF));
 						strncpy(adr[M-1].namesurname, name, N);
 						printf("Номер телефона:\n");
-						scanf("%d%*c", phone);
-						adr[M-1].phonenum = *phone;
+						scn=scanf("%d%*c", phone);
+						if (scn!=1)
+							{
+								printf("You died\n");
+								return 0;
+							}
+						adr[M-1].phonenum=*phone;
 						printf("Добавлен абонент:\n%s% d\n", adr[M-1].namesurname, adr[M-1].phonenum);
 						break;
 							
@@ -113,7 +90,7 @@ int main()
 					
 				case 2: //список абонентов
 					{
-					
+						printf("Список абонентов:\n");					
 						for (i=0; i<M; ++i)
 							{
 								printf("%s %d\n", adr[i].namesurname, adr[i].phonenum);
@@ -124,7 +101,13 @@ int main()
 				case 3: //поиск абонента
 					{
 					    printf("Номер телефона\n");
-						scanf("%d%*c", &phs);
+						scn=scanf("%d%*c", &phs);
+						if (scn!=1)
+							{
+								printf("You died\n");
+								return 0;
+							}
+
 
 							for (i=0; i<M; ++i)
 							{
@@ -132,7 +115,8 @@ int main()
 								{
 									printf("Имя фамилия: %s\nНомер телефона: %d\n", adr[i].namesurname, adr[i].phonenum);
 								}
-							}
+							}		
+						printf("Не найден =(\n");
 						break;
 					}
 
@@ -140,42 +124,51 @@ int main()
 
 				case 4: //удаление абонента
 					{
-						int k;
 						printf("Выберете номер для удаления:\n");
-
 							for (i=0; i<M; ++i)
 							{
 								printf("%d. %s %d\n", i+1, adr[i].namesurname, adr[i].phonenum); 
 							}
-
-						scanf("%d%*c", &k);
-						
-						if ((k <= 0)||(k>M))
-
+						int k;
+						scn=scanf("%d%*c", &k);
+						if (scn!=1)
 							{
-							printf("try again\n");
-							break;
+								printf("You died\n");
+								return 0;
+							}
+						if ((k<=0)||(k>M))
+							{
+								printf("try again\n");
+								break;
 							}
 						--k;
-						strncpy(adr[k].namesurname, "0", N);
-						adr[k].phonenum=0;
+						memmove(&adr[k], &adr[k+1], sizeof(book)*(M-k));
+						--M;
+						adr=realloc(adr, sizeof(book)*M);
+						if (adr==NULL)
+							{
+								free(adr);
+								printf("ошибка выделения памяти\n");
+								break;
+							}
+						printf("Абонент удален!\n");
 					}
-
 					break;
 
 				case 5: //Выход
 					{
 						return 0;
+						free(adr);
 					}
 
 					break;
 
 				default:
 					{
-						printf("Ошибка\n");
+						printf("Неизвестный пункт меню\n");
 						break;
 					}
-			} 
+		} 
 	}
 	free(adr);		
 	return 0;
